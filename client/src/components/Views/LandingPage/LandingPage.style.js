e06d75ae40e29eaa82ad26807ff7d1b8fe047e14
Eiosx <eiosxenzo@gmail.com>
2019-05-30 20:09:18 -0400

    Added Landing page view, sortby component, searchbar component. Need to finish functionality of sortby component. Need to add react router and hookstore.

---
+++ client/src/components/Views/LandingPage/LandingPage.style.js
@@ -0,0 +1,65 @@
+
+export const styles = {
+  discussionsContainer: {
+    // border: '2px solid black',
+    width: '60%',
+    margin: '200px auto',
+    display: 'flex',
+    borderRadius: '3px',
+    flexWrap: 'wrap',
+    backgroundColor: '#eaeaea'
+  },
+  topDiscussions: {
+    // border: '2px solid green',
+    display: 'flex',
+    margin: '20px auto',
+    flexWrap: 'wrap',
+    width: '100%',
+    // border: '1px solid black',
+  },
+  topDiscussionsHead: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    // border: '1px solid pink',
+    width: '90%',
+    padding: '10px 0%',
+    borderBottom: '1px solid black',
+    margin: '0px auto'
+  },
+  sortBy: {
+    marginTop: '5px'
+  },
+  topDiscussionsTitle: {
+    fontSize: '40px'
+  },
+  discussion: {
+    // border: '1px solid blue',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    margin: '10px 5%',
+    width: '100%',
+    lineHeight: '1.2'
+  }, 
+  discussionTitle: {
+    fontWeight: 'bold',
+    fontSize: '20px'
+  },
+  subtopic: {
+    fontWeight: 'bold',
+    fontSize: '12px'
+  },
+  comments: {
+    display: 'flex',
+    flexDirection: 'column',
+    // border: '1px solid black'
+  },
+  commentsNumber: {
+    alignSelf: 'center',
+    margin: '10px auto',
+    fontWeight: 'bold',
+    fontSize: '20px'
+  },
+  logo: {
+  }
+};
