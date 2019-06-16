7c8ab02eae379628b5558fd713f1fafabf7afae8
Eiosx <eiosxenzo@gmail.com>
2019-06-15 22:38:32 -0400

    Added followers page

--- client/src/components/Views/SubForum/SubForumContainer.js
+++ client/src/components/Views/SubForum/SubForumContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   withStyles, Typography, Grid, Button, ButtonGroup,
   Icon, IconButton
@@ -7,9 +7,22 @@ import { styles } from './SubForm.style';
 import Discussions from '../../Modules/Discussions';
 import SortBy from '../../PersistentComponents/SortBy/SortBy';
 import { Link } from 'react-router-dom';
+import Followers from './Followers';
 
 export default withStyles(styles)(props => {
   const { classes } = props;
+  const [hideDiscussions, updateHideDiscussions] = useState(false);
+  const [hideFollowers, updateHideFollowers] = useState(true);
+
+  const handleHide = (view, e) => {
+    if (view === 'discussions') {
+      updateHideDiscussions(false);
+      updateHideFollowers(true);
+    } else {
+      updateHideDiscussions(true);
+      updateHideFollowers(false);
+    }
+  }
 
   //will make axios call and input subtopic id from url params
   return (
@@ -18,16 +31,22 @@ export default withStyles(styles)(props => {
         <Typography variant="h3">/d/SubForm</Typography>
         <Button variant="contained" color='secondary'>Join!</Button>
         <ButtonGroup variant="contained">
-          <Link to={`/subForum/${1}/discussions`}>
-            <Button>Discussions</Button>
-          </Link>
-          <Link to={`/subForum/${1}/followers`}>
-            <Button>Followers</Button>
-          </Link>
+          {/* <Link to={`/subForum/${1}/discussions`}> */}
+          <Button onClick={(e) => handleHide('discussions', e)}>Discussions</Button>
+          {/* </Link> */}
+          {/* <Link to={`/subForum/${1}/followers`}> */}
+          <Button onClick={(e) => handleHide('followers', e)}>Followers</Button>
+          {/* </Link> */}
         </ButtonGroup>
         <SortBy classes={classes} />
       </Grid>
-      <Discussions view='subForum' />
+      {!hideDiscussions ?
+        <Discussions view='subForum' />
+        : null}
+      {!hideFollowers ?
+        <Followers />
+        : null}
+
     </Grid>
   )
 })
