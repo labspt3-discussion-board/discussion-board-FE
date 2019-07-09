232e49e48491a9ab543789083c7a2ac677db2dc0
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-07-08 20:02:30 -0500

    Added payment post request

--- client/src/components/Views/Payment/CheckoutForm.js
+++ client/src/components/Views/Payment/CheckoutForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
+import axios from 'axios';
 
 class CheckoutForm extends Component {
   constructor(props) {
@@ -8,8 +9,35 @@ class CheckoutForm extends Component {
   }
 
   async submit(ev) {
-    // User clicked submit
-  }
+  let {token} = await this.props.stripe.createToken();
+  console.log(token);
+
+  // axios
+  //   .post('http://127.0.0.1:8000/api/payment/', token)
+  //   .then(res => {
+  //     console.log(token);
+  //     console.log(res.data);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+
+    axios(
+      {
+        method: 'post',
+        url: `http://127.0.0.1:8000/api/payment/`,
+        withCredentials: true,
+        headers: {
+          //'Authorization': 'Bearer ' + localStorage.getItem('LAMBDA_FORUM_AUTH_TOKEN'),
+          'Authorization': 'Token ' + '5141e0976c89617c73d220a2aab647099dacad97',
+        },
+        data: {
+          stripetoken: token.id,
+        }
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+}
 
   render() {
     return (
